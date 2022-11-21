8069d075e97c3e81c7d394ec76f7fc320b1acf84
leo-cerdan <leo.cerdan@gmail.com>
2022-11-21 21:09:49 +0100

    Final ejercicio 1

--- Simple/ejercicio1.py
+++ Simple/ejercicio1.py
@@ -15,6 +15,18 @@ def genera_diccionario():
 
 def ordenar_diccionario(diccionario):
 
+    """
+    Test de la función ordenar_diccionario
+    >>> ordenar_diccionario({'A': 72, 'B': 71, 'C': 81, 'D': 55, 'E': 37, 'F': 89, 'G': 31, 'H': 21, 'I': 18, 'J': 58})
+    La persona más joven tiene el id: I y su edad es: 18 
+    La persona más anciana tiene el id: F y su edad es: 89
+    {'I': 18, 'H': 21, 'G': 31, 'E': 37, 'D': 55, 'J': 58, 'B': 71, 'A': 72, 'C': 81, 'F': 89}
+    >>> ordenar_diccionario({'A': 72, 'B': 71, 'C': 81, 'D': 55, 'E': 37, 'F': 89, 'G': 31, 'H': 21, 'I': 18, 'J': 58})
+    La persona más joven tiene el id: I y su edad es: 18 
+    La persona más anciana tiene el id: F y su edad es: 89
+    {'A': 72, 'B': 71, 'C': 81, 'D': 55, 'E': 37, 'F': 89, 'G': 31, 'H': 21, 'I': 18, 'J': 58}
+    """
+
     ordenado = dict(sorted(diccionario.items(), key=itemgetter(1)))
 
     bajo = list(ordenado.keys())[0]
@@ -23,4 +35,8 @@ def ordenar_diccionario(diccionario):
     print(f"La persona más joven tiene el id: {bajo} y su edad es: {ordenado[bajo]} ")
     print(f"La persona más anciana tiene el id: {alto} y su edad es: {ordenado[alto]}")
 
+    return ordenado
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
