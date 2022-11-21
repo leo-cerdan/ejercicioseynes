154d5191b33ec96c37558a14e44da1828c59c135
leo-cerdan <leo.cerdan@gmail.com>
2022-11-21 21:56:10 +0100

    Final ejercicio 2

--- Matriz/ejercicio2.py
+++ Matriz/ejercicio2.py
@@ -9,6 +9,14 @@ def crear_matriz():
     return matriz
 
 def comprobar_horizontal(matriz):
+    """
+    Test función comprobar_horizontal
+    >>> comprobar_horizontal([[20, 21, 22, 23, 16], [21, 24, 17, 10, 23], [22, 17, 18, 19, 20], [23, 21, 19, 10, 4], [17, 7, 20, 3, 22]])
+    Horizontal: inicia con el número 20 posición: matriz[0][0], finaliza con el número: 23 posición: matriz[0][3]
+    Horizontal: inicia con el número 17 posición: matriz[2][1], finaliza con el número: 20 posición: matriz[2][4]
+    >>> comprobar_horizontal([[2, 5, 9, 10, 1], [20, 4, 24, 10, 23], [14, 5, 20, 8, 20], [23, 6, 19, 10, 4], [17, 7, 8, 9, 10]])
+    Horizontal: inicia con el número 7 posición: matriz[4][1], finaliza con el número: 10 posición: matriz[4][4]
+    """
     horizontal = 0
     horizontal1 = 0
     horizontal2 = 0
@@ -39,6 +47,14 @@ def comprobar_horizontal(matriz):
                         resultado_horizontal(matriz, i, j)
 
 def comprobar_vertical(matriz):
+    """
+    Test función comprobar_vertical:
+    >>> comprobar_vertical([[20, 21, 22, 23, 16], [21, 24, 17, 10, 23], [22, 17, 18, 19, 20], [23, 21, 19, 10, 4], [17, 7, 20, 3, 22]])
+    Vertical: inicia con el número 20 posición: matriz[0][0], finaliza con el número: 23 posición: matriz[3][0]
+    Vertical: inicia con el número 17 posición: matriz[1][2], finaliza con el número: 20 posición: matriz[4][2]
+    >>> comprobar_vertical([[2, 5, 9, 10, 1], [20, 4, 24, 10, 23], [14, 5, 20, 8, 20], [23, 6, 19, 10, 4], [17, 7, 8, 9, 10]])
+    Vertical: inicia con el número 4 posición: matriz[1][1], finaliza con el número: 7 posición: matriz[4][1]
+    """
     vertical = 0
     vertical1 = 0
     vertical2 = 0
@@ -74,3 +90,11 @@ def resultado_horizontal(matriz, i, j):
 def resultado_verical(matriz, i, j):
     print(f"Vertical: inicia con el número {matriz[j-2][i]} posición: matriz[{j-2}][{i}], finaliza con el número: {matriz[j+1][i]} posición: matriz[{j+1}][{i}]")
 
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
+    matriz = crear_matriz()
+    print(matriz)
+    comprobar_horizontal(matriz)
+    comprobar_vertical(matriz)
