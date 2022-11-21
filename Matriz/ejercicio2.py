f401388d7416614b8790eda61a21b00482f55931
leo-cerdan <leo.cerdan@gmail.com>
2022-11-21 21:33:17 +0100

    Creación función comprobar matriz vertical

--- Matriz/ejercicio2.py
+++ Matriz/ejercicio2.py
@@ -38,6 +38,39 @@ def comprobar_horizontal(matriz):
                     if horizontal4 == 3:
                         resultado_horizontal(matriz, i, j)
 
+def comprobar_vertical(matriz):
+    vertical = 0
+    vertical1 = 0
+    vertical2 = 0
+    vertical3 = 0
+    vertical4 = 0
+    for i in range(5):
+        for j in range(4):
+            if matriz[j][i]+1 == matriz[j+1][i]:
+                if i == 0:
+                    vertical += 1
+                    if vertical == 3:
+                        resultado_verical(matriz, i, j)
+                elif i == 1:
+                    vertical1 += 1
+                    if vertical1 == 3:
+                        resultado_verical(matriz, i, j)
+                elif i == 2:
+                    vertical2 += 1
+                    if vertical2 == 3:
+                        resultado_verical(matriz, i, j)
+                elif i == 3:
+                    vertical3 += 1
+                    if vertical3 == 3:
+                        resultado_verical(matriz, i, j)
+                elif i == 4:
+                    vertical4 += 1
+                    if vertical4 == 3:
+                        resultado_verical(matriz, i, j)
+
 def resultado_horizontal(matriz, i, j):
     print(f"Horizontal: inicia con el número {matriz[i][j-2]} posición: matriz[{i}][{j-2}], finaliza con el número: {matriz[i][j+1]} posición: matriz[{i}][{j+1}]")
+
+def resultado_verical(matriz, i, j):
+    print(f"Vertical: inicia con el número {matriz[j-2][i]} posición: matriz[{j-2}][{i}], finaliza con el número: {matriz[j+1][i]} posición: matriz[{j+1}][{i}]")
 
